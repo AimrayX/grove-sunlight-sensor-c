2e93c491c5e79f5eba59cb98c9c26f4f7f91f704
AimrayX <timothy.hall@gmx.ch>
2025-07-10 18:04:28 +0200

    added settings, adjusted macro names, adjusted functions and function names, added param read and write functionality, started default init

--- si1145.c
+++ si1145.c
@@ -19,9 +19,22 @@ void si1145_reset(si1145_t *dev) {
 
 
 
-void si1145_de_init(si1145_t *dev) {
-
-
+void si1145_default_init(si1145_t *dev) {
+    si1145_write_to_register(dev, SI1145_REG_UCOEF0, 0x29);
+    si1145_write_to_register(dev, SI1145_REG_UCOEF1, 0x89);
+    si1145_write_to_register(dev, SI1145_REG_UCOEF2, 0x02);
+    si1145_write_to_register(dev, SI1145_REG_UCOEF3, 0x00);
+    si1145_write_param_data(dev, SI1145_PARAM_CHLIST, SI1145_PARAM_CHLIST_ENUV 
+                               | SI1145_PARAM_CHLIST_ENALSIR 
+                               | SI1145_PARAM_CHLIST_ENALSVIS
+                               | SI1145_PARAM_CHLIST_ENPS1);
+    
+    si1145_write_param_data(dev, SI1145_PARAM_PS1_ADCMUX, SI1145_ADCMUX_LARGE_IR);
+    si1145_write_to_register(dev, SI1145_REG_PS_LED21, SI1145_SET_LED_CURRENT_22MA);
+    
+    si1145_write_param_data(dev, SI1145_PARAM_PS_ADC_GAIN, SI1145_SET_ADC_GAIN_DIV1);
+    si1145_write_param_data(dev, SI1145_PARAM_PS_ADC_COUNTER, SI1145_SET_ADC_COUNTER_511ADCCLK);
+    si1145_write_param_data(dev, SI1145_PARAM_PS_ADC_MISC, SI1145_SET_ADC_MISC_HIGHRANGE);
 }
 
 
@@ -44,7 +57,7 @@ int si1145_write_to_register(si1145_t *dev, uint8_t reg, uint8_t value) {
 
 
 
-int si1145_param_set(si1145_t *dev, uint8_t value) {
+int si1145_exec_command(si1145_t *dev, uint8_t value) {
     int i = 0;
     int j = 0;
     struct timespec ts1 = {0, 25000000}; // 0 seconds, 25'000'000 nanoseconds (25ms)
@@ -55,7 +68,8 @@ int si1145_param_set(si1145_t *dev, uint8_t value) {
         while (i < 2)
         {
             //1. Write 0x00 to Command register to clear the Response register
-            wiringPiI2CWriteReg8(dev->i2c_fd, SI1145_REG_COMMAND, SI1145_NOP);
+            wiringPiI2CWriteReg8(dev->i2c_fd, SI1145_REG_COMMAND, SI1145_CMD_NOP);
+
             //2. Read Response register and verify contents are 0x00
             if(si1145_read_from_register(dev, SI1145_REG_RESPONSE) != 0x00) {
                 fprintf(stderr, "Warning: Response register not 0x00 after 1. write to command register\n");
@@ -80,17 +94,17 @@ int si1145_param_set(si1145_t *dev, uint8_t value) {
             //Response register after reset. 
             //No Commands should be issued to the device for at least 1 ms after 
             //a Reset is issued.
-            if (value == SI1145_RESET)
+            if (value == SI1145_CMD_RESET)
             {
                 nanosleep(&ts2, NULL);
                 break;
             }
             
-            nanosleep(&ts1, NULL);
             //4. Read the Response register and verify contents are now non-zero. 
             //If contents are still 0x00, repeat this step.
             //If the Response register remains 0x00 for over 25 ms after the Command write, 
             //the entire Command process should be repeated starting with Step 1.
+            nanosleep(&ts1, NULL);
             if(si1145_read_from_register(dev, SI1145_REG_RESPONSE) == 0x00) {
                 fprintf(stderr, "Warning: Failed to write command to command register\n");
             }
@@ -122,16 +136,17 @@ int si1145_ps_als_force(si1145_t *dev) {
 
 
 
-uint8_t si1145_read_param_data(si1145_t *dev, uint8_t reg) {
-
-
+uint8_t si1145_read_param_data(si1145_t *dev, uint8_t param) {
+    si1145_exec_command(dev, SI1145_CMD_PARAM_QUERY | param);
+    return si1145_read_from_register(dev, SI1145_REG_PARAM_RD);
 }
 
 
 
-uint8_t si1145_write_param_data(si1145_t *dev, uint8_t reg, uint8_t value) {
-
-
+uint8_t si1145_write_param_data(si1145_t *dev, uint8_t param, uint8_t value) {
+    si1145_write_to_register(dev, SI1145_REG_PARAM_WR, value);
+    si1145_exec_command(dev, SI1145_CMD_PARAM_SET | param);
+    return si1145_read_from_register(dev, SI1145_REG_PARAM_RD);
 }
 
 
@@ -158,27 +173,6 @@ uint16_t si1145_read_proximity(si1145_t *dev, int PSn) {
 
 
 uint16_t si1145_read_UV(si1145_t *dev) {
-
-
-}
-
-
-
-int si1145_write_byte(si1145_t *dev, uint8_t reg, uint8_t value) {
-
-
-}
-
-
-
-int si1145_read_byte(si1145_t *dev, uint8_t reg) {
-
-
-}
-
-
-
-int si1145_read_half_word(si1145_t *dev, uint8_t reg) {
 
 
 }
--- si1145.h
+++ si1145.h
@@ -17,19 +17,16 @@ typedef struct {
 
 bool si1145_begin(si1145_t *dev);
 void si1145_reset(si1145_t *dev);
-void si1145_de_init(si1145_t *dev);
+void si1145_default_init(si1145_t *dev);
 uint8_t si1145_read_from_register(si1145_t *dev, uint8_t reg);
 int si1145_write_to_register(si1145_t *dev, uint8_t reg, uint8_t value);
-int si1145_param_set(si1145_t *dev, uint8_t value);
+int si1145_exec_command(si1145_t *dev, uint8_t value);
 int si1145_ps_als_force(si1145_t *dev);
-uint8_t si1145_read_param_data(si1145_t *dev, uint8_t reg);
-uint8_t si1145_write_param_data(si1145_t *dev, uint8_t reg, uint8_t value);
+uint8_t si1145_read_param_data(si1145_t *dev, uint8_t param);
+uint8_t si1145_write_param_data(si1145_t *dev, uint8_t param, uint8_t value);
 uint16_t si1145_read_visible(si1145_t *dev);
 uint16_t si1145_read_IR(si1145_t *dev);
 uint16_t si1145_read_proximity(si1145_t *dev, int PSn);
 uint16_t si1145_read_UV(si1145_t *dev);
-int si1145_write_byte(si1145_t *dev, uint8_t reg, uint8_t value);
-int si1145_read_byte(si1145_t *dev, uint8_t reg);
-int si1145_read_half_word(si1145_t *dev, uint8_t reg);
 
 #endif
