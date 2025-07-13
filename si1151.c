5093aa07345949001ef00743c53146241f8e1be9
AimrayX <timothy.hall@gmx.ch>
2025-07-13 17:41:35 +0200

    finished functions, updated documentation and some small other things

--- si1151.c
+++ si1151.c
@@ -5,64 +5,55 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-int si1151_begin(si1151_t *dev) {
+int si1151_begin(si1151_t *dev, bool mode) {
+    dev->is_autonomous = mode;
     dev->i2c_fd = wiringPiI2CSetup(SI1151_ADDR);
     dev->part_ID = si1151_read_from_register(dev, SI1151_REG_PART_ID);
     si1151_reset(dev);
     si1151_default_init(dev);
+    si1151_exec_command(dev, SI1151_CMD_START);
 }
 
 void si1151_reset(si1151_t *dev) {
-    si1151_write_to_register(dev, SI1151_REG_MEAS_RATE0, 0);
-    si1151_write_to_register(dev, SI1151_REG_MEAS_RATE1, 0);
-    si1151_write_to_register(dev, SI1151_REG_IRQ_ENABLE, 0);
-    si1151_write_to_register(dev, SI1151_REG_INT_CFG, 0);
-    si1151_write_to_register(dev, SI1151_REG_IRQ_STATUS, 0);
-    si1151_exec_command(dev, SI1151_CMD_RESET);
-    sleep(10);
-    si1151_write_to_register(dev, SI1151_REG_HW_KEY, 0x17);
-    delay(10);
+    si1151_exec_command(dev, SI1151_CMD_RESET_SW);
 }
 
 void si1151_default_init(si1151_t *dev) {
-    si1151_write_to_register(dev, SI1151_REG_UCOEF0, 0x29);
-    si1151_write_to_register(dev, SI1151_REG_UCOEF1, 0x89);
-    si1151_write_to_register(dev, SI1151_REG_UCOEF2, 0x02);
-    si1151_write_to_register(dev, SI1151_REG_UCOEF3, 0x00);
-    si1151_write_param_data(dev, SI1151_PARAM_CHLIST, 
-                                 SI1151_PARAM_CHLIST_EN_UV 
-                               | SI1151_PARAM_CHLIST_EN_ALS_IR 
-                               | SI1151_PARAM_CHLIST_EN_ALS_VIS
-                               | SI1151_PARAM_CHLIST_EN_PS1);
+    si1151_write_param_data(dev, SI1151_PARAM_CHAN_LIST, SI1151_SET_CHAN_LIST_CHAN0_EN 
+                                                       | SI1151_SET_CHAN_LIST_CHAN1_EN);
     
+    si1151_write_to_register(dev, SI1151_REG_IRQ_ENABLE, SI1151_SET_IRQ_ENABLE_IE0
+                                                       | SI1151_SET_IRQ_ENABLE_IE1);
+
     //set LED1 current
     //not sure if really needed
-    si1151_write_param_data(dev, SI1151_PARAM_PS1_ADCMUX, SI1151_ADCMUX_LARGE_IR);
-    si1151_write_to_register(dev, SI1151_REG_PS_LED21, SI1151_SET_LED_CURRENT_22MA);
-    si1151_write_param_data(dev, SI1151_PARAM_PSLED12_SELECT, SI1151_PSLED12_SELECT_PS1_LED1);
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_RATE_H,  0);
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_RATE_L,  1);
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_COUNT0,  1);
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_COUNT1,  1);
+    si1151_write_param_data(dev, SI1151_PARAM_THRESHOLD0_L, 0);
+    si1151_write_param_data(dev, SI1151_PARAM_THRESHOLD0_H, 0);
+
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_CONFIG0, SI1151_SET_ADC_CONF_MUX_SMALL_IR 
+                                                         | SI1151_SET_ADC_CONF_DECIM_RATE_512);
+
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_CONFIG1, SI1151_SET_ADC_CONF_MUX_VISIBLE 
+                                                         | SI1151_SET_ADC_CONF_DECIM_RATE_512);
+
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_SENS0, SI1151_SET_ADC_SENS_HW_GAIN1);
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_SENS1, SI1151_SET_ADC_SENS_HW_GAIN1);
+
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_POST0, SI1151_SET_ADC_POST_THRESH_EN1);
+    si1151_write_param_data(dev, SI1151_PARAM_ADC_POST1, SI1151_SET_ADC_POST_THRESH_EN1);
+
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_CONFIG0, SI1151_SET_MEAS_CONF_LED1_EN 
+                                                          | SI1151_SET_MEAS_CONF_BANK_SEL_LEDX_A 
+                                                          | SI1151_SET_MEAS_CONF_COUNTER_INDEX0);
+
+    si1151_write_param_data(dev, SI1151_PARAM_MEAS_CONFIG1, SI1151_SET_MEAS_CONF_LED2_EN 
+                                                          | SI1151_SET_MEAS_CONF_BANK_SEL_LEDX_B
+                                                          | SI1151_SET_MEAS_CONF_COUNTER_INDEX1);
     
-    //adc settings
-    si1151_write_param_data(dev, SI1151_PARAM_PS_ADC_GAIN, SI1151_SET_ADC_GAIN_DIV1);
-    si1151_write_param_data(dev, SI1151_PARAM_PS_ADC_COUNTER, SI1151_SET_ADC_COUNTER_511ADCCLK);
-    si1151_write_param_data(dev, SI1151_PARAM_PS_ADC_MISC, SI1151_SET_ADC_MISC_HIGHRANGE);
-
-    //vis settings
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_VIS_ADC_GAIN, SI1151_SET_ADC_GAIN_DIV1);
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_VIS_ADC_COUNTER, SI1151_SET_ADC_COUNTER_511ADCCLK);
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_VIS_ADC_MISC, SI1151_SET_ADC_MISC_HIGHRANGE);
-
-    //ir settings
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_IR_ADC_GAIN, SI1151_SET_ADC_GAIN_DIV1);
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_IR_ADC_COUNTER, SI1151_SET_ADC_COUNTER_511ADCCLK);
-    si1151_write_param_data(dev, SI1151_PARAM_ALS_IR_ADC_MISC, SI1151_SET_ADC_MISC_HIGHRANGE);
-
-    //enable interrupt
-    si1151_write_to_register(dev, SI1151_REG_INT_CFG, 0x01);
-    si1151_write_to_register(dev, SI1151_REG_IRQ_ENABLE, SI1151_SET_IRQ_ENABLE_ALS_IE);
-
-    //auto run
-    si1151_write_to_register(dev, SI1151_REG_MEAS_RATE0, 0xFF);
-    si1151_exec_command(dev, SI1151_CMD_PSALS_AUTO);
 }
 
 uint8_t si1151_read_from_register(si1151_t *dev, uint8_t reg) {
@@ -71,6 +62,12 @@ uint8_t si1151_read_from_register(si1151_t *dev, uint8_t reg) {
 
 int si1151_write_to_register(si1151_t *dev, uint8_t reg, uint8_t value) {
     
+    //This is necessary because this register behaves like a param setting
+    if (reg == SI1151_REG_IRQ_ENABLE)
+    {
+        value = value ^ si1151_read_from_register(dev, reg);
+    }
+
     if(wiringPiI2CWriteReg8(dev->i2c_fd, reg, value) != 0) {
         fprintf(stderr, "[ERROR] %s:%d: Failed to write to register\n", __FILE__, __LINE__);
         return -1;
@@ -120,6 +117,18 @@ int si1151_exec_command(si1151_t *dev, uint8_t value) {
         return -1;
     }
 
+    //wait until reset command has finished executing
+    if (value == SI1151_CMD_RESET_SW)
+    {
+        while (si1151_read_from_register(dev, SI1151_REG_RESPONSE0) != 0x0F)
+        {
+            fprintf(stderr, "[WARNING] %s:%d: Reset hasn't finished, waiting...\n", __FILE__, __LINE__);
+            sleep(5);
+        }
+        fprintf(stderr, "[INFO] %s:%d: Sensor has been reset\n", __FILE__, __LINE__);
+        return 0;
+    }
+
     if(si1151_read_from_register(dev, SI1151_REG_RESPONSE0) == (dev->cmd_ctr)) {
         fprintf(stderr, "[ERROR] %s:%d: Wrote to register without the cmd counter changing\n", __FILE__, __LINE__);
         return -1;
@@ -145,41 +154,19 @@ uint8_t si1151_write_param_data(si1151_t *dev, uint8_t param, uint8_t value) {
 }
 
 uint16_t si1151_read_visible(si1151_t *dev) {
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_ALS_VIS_DATA0);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_ALS_VIS_DATA1);
+    if(!dev->is_autonomous) si1151_exec_command(dev, si1151_force_measurement);
+
+    uint8_t low = si1151_read_from_register(dev, SI1151_REG_HOST_OUT0);
+    uint8_t high = si1151_read_from_register(dev, SI1151_REG_HOST_OUT1);
     uint16_t rslt = ((uint16_t)high << 8) | low;
     dev->IR = rslt;
     
     return rslt;
 }
-
-
 
 uint16_t si1151_read_IR(si1151_t *dev) {
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_ALS_IR_DATA0);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_ALS_IR_DATA1);
-    uint16_t rslt = ((uint16_t)high << 8) | low;
-    dev->IR = rslt;
-    
-    return rslt;
-}
-
-
-
-uint16_t si1151_read_proximity(si1151_t *dev, int PSn) {
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_PS1_DATA0);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_PS1_DATA1);
-    uint16_t rslt = ((uint16_t)high << 8) | low;
-    dev->IR = rslt;
-    
-    return rslt;
-}
-
-
-
-uint16_t si1151_read_UV(si1151_t *dev) {
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_UVINDEX0);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_UVINDEX1);
+    uint8_t low = si1151_read_from_register(dev, SI1151_REG_HOST_OUT2);
+    uint8_t high = si1151_read_from_register(dev, SI1151_REG_HOST_OUT3);
     uint16_t rslt = ((uint16_t)high << 8) | low;
     dev->IR = rslt;
     
--- si1151.h
+++ si1151.h
@@ -10,6 +10,7 @@ typedef struct {
     uint8_t part_ID;
     uint8_t i2c_addr;
     int i2c_fd;
+    bool is_autonomous;
     uint8_t cmd_ctr;
     bool initialized;
     uint8_t last_error;
@@ -25,9 +26,10 @@ typedef struct {
  *
  * 
  * @param dev Device pointer to si1151 struct
+ * @param mode 1 for autonomous, 0 for forced
  * @return Returns 0 if normal -1 for error
  */
-int si1151_begin(si1151_t *dev);
+int si1151_begin(si1151_t *dev, bool mode);
 
 /**
  * @brief Resets sensor
@@ -55,9 +57,21 @@ void si1151_default_init(si1151_t *dev);
  * 
  * @param dev Device pointer to si1151 struct
  * @param reg Register to read from
- * @return Returns read value
+ * @return Returns read 8-Bit value
  */
 uint8_t si1151_read_from_register(si1151_t *dev, uint8_t reg);
+
+/**
+ * @brief Write to a register
+ * 
+ * Writes a value to a register via the i2c protocol
+ *
+ * 
+ * @param dev Device pointer to si1151 struct
+ * @param reg Register to write to
+ * @param value Data to write
+ * @return Returns 0 if normal -1 for error
+ */
 int si1151_write_to_register(si1151_t *dev, uint8_t reg, uint8_t value);
 
 /**
@@ -69,12 +83,13 @@ int si1151_write_to_register(si1151_t *dev, uint8_t reg, uint8_t value);
  * and thus the command didn't execute
  * 
  * @param dev Device pointer to si1151 struct
+ * @param value Command to execute
  * @return Returns 0 if normal -1 for error
  */
 int si1151_exec_command(si1151_t *dev, uint8_t value);
 
 /**
- * @brief Forces a single PS and ALS measurement
+ * @brief Forces a single IR and VIS measurement
  *
  * 
  * @param dev Device pointer to si1151 struct
@@ -87,27 +102,48 @@ int si1151_force_measurement(si1151_t *dev);
  * 
  * Reads from a register via the i2c protocol 
  * by first executing the query command with the given param
- * and then reading the PARAM_RD register 
+ * and then reading the RESPONE1 register 
  * 
  * @param dev Device pointer to si1151 struct
- * @param param Param to read
- * @return Returns read value
+ * @param param Internal register to read
+ * @return Returns read 8-Bit value
  */
 uint8_t si1151_read_param_data(si1151_t *dev, uint8_t param);
 
-
+/**
+ * @brief Write to a internal param register
+ * 
+ * Writes to a internal param register via the i2c protocol 
+ * by first writing to the HOST_IN0 register
+ * and then executing the PARAM_SET command
+ * 
+ * @param dev Device pointer to si1151 struct
+ * @param param Internal register to write to
+ * @param value Data to write
+ * @return Returns the set 8-Bit value
+ */
 uint8_t si1151_write_param_data(si1151_t *dev, uint8_t param, uint8_t value);
 
-
+/**
+ * @brief Read visible light data
+ * 
+ * Reads from the HOST_OUT0/1 registers via the i2c protocol 
+ * directly or if in forced mode by first triggering a measurement
+ * 
+ * @param dev Device pointer to si1151 struct
+ * @return Returns 16-Bit read value
+ */
 uint16_t si1151_read_visible(si1151_t *dev);
 
-
+/**
+ * @brief Read infrared light data
+ * 
+ * Reads from the HOST_OUT2/3 registers via the i2c protocol 
+ * directly or if in forced mode by first triggering a measurement
+ * 
+ * @param dev Device pointer to si1151 struct
+ * @return Returns 16-Bit read value
+ */
 uint16_t si1151_read_IR(si1151_t *dev);
-
-
-uint16_t si1151_read_proximity(si1151_t *dev, int PSn);
-
-
-uint16_t si1151_read_UV(si1151_t *dev);
 
 #endif
