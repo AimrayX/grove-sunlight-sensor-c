d3266a028371c0f848302a39b9ee853ddcfa96ee
AimrayX <timothy.hall@gmx.ch>
2025-08-05 10:21:22 +0200

    finished refactoring i2c protocol

--- si1151.c
+++ si1151.c
@@ -2,16 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
 
-int si1151_begin(si1151_t *dev, bool mode) {
+int si1151_begin(si1151_t *dev, bool mode, int fd) {
     dev->is_autonomous = mode;
-    dev->i2c_fd = wiringPiI2CSetup(SI1151_ADDR);
-    dev->part_ID = si1151_read_from_register(dev, SI1151_REG_PART_ID);
+    dev->i2c_fd = fd;
+
+    uint8_t data = 0;
+    si1151_read_from_register(dev, SI1151_REG_PART_ID, &data);
+    dev->part_ID = data;
+
     si1151_reset(dev);
     si1151_default_init(dev);
     fprintf(stderr, "[INFO] %s:%d: Sensor has been set to default settings\n", __FILE__, __LINE__);
+    
     si1151_exec_command(dev, SI1151_CMD_START);
     fprintf(stderr, "[INFO] %s:%d: Starting measurements\n", __FILE__, __LINE__);
     return 0;
@@ -59,33 +65,67 @@ void si1151_default_init(si1151_t *dev) {
     
 }
 
-uint8_t si1151_read_from_register(si1151_t *dev, uint8_t reg) {
-    return wiringPiI2CReadReg8(dev->i2c_fd, reg);
+int si1151_read_from_register(si1151_t *dev, uint8_t reg, uint8_t *data) {
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
+
+    messages[0].addr  = dev->i2c_addr;
+    messages[0].flags = 0;
+    messages[0].len   = 1;
+    messages[0].buf   = &reg;
+
+    messages[1].addr  = dev->i2c_addr;
+    messages[1].flags = I2C_M_RD;
+    messages[1].len   = 1;
+    messages[1].buf   = data;
+
+    packets.msgs  = &messages;
+    packets.nmsgs = 2;
+
+    if (ioctl(dev->i2c_fd, I2C_RDWR, &packets) < 0) {
+        fprintf(stderr, "[ERROR] %s:%d: Failed to read register\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    return 0;
 }
 
 int si1151_write_to_register(si1151_t *dev, uint8_t reg, uint8_t value) {
-    
+    uint8_t buffer[2];
+    buffer[0] = reg;
+    buffer[1] = value;
+
     //This is necessary because this register behaves like a param setting
     if (reg == SI1151_REG_IRQ_ENABLE)
     {
-        value = value ^ si1151_read_from_register(dev, reg);
+        uint8_t current = 0;
+        if (si1151_read_from_register(dev, reg, &current) == 0) {
+            return -1;
+        }
+        value = value | current;
     }
 
-    if(wiringPiI2CWriteReg8(dev->i2c_fd, reg, value) != 0) {
-        fprintf(stderr, "[ERROR] %s:%d: Failed to write to register\n", __FILE__, __LINE__);
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[1];
+
+    messages[0].addr  = dev->i2c_addr;
+    messages[0].flags = 0;
+    messages[0].len   = 2;
+    messages[0].buf   = buffer;
+
+    packets.msgs  = &messages;
+    packets.nmsgs = 1;
+
+    if (ioctl(dev->i2c_fd, I2C_RDWR, &packets) < 0) {
+        fprintf(stderr, "[ERROR] %s:%d: Failed to read register\n", __FILE__, __LINE__);
         return -1;
     }
-    sleep(1);
-    uint8_t rs = si1151_read_from_register(dev, reg);
-    if (reg != SI1151_REG_COMMAND && si1151_read_from_register(dev, reg) != value)
-    {
-        fprintf(stderr, "[ERROR] %s:%d: Wrote to register, but check returned wrong value\n", __FILE__, __LINE__);
-        return -1;
-    }
+
     return 0;
 }
 
 int si1151_exec_command(si1151_t *dev, uint8_t value) {
+    uint8_t data = 0;
     int i = 0;
     while (i < 2)
     {
@@ -101,8 +141,9 @@ int si1151_exec_command(si1151_t *dev, uint8_t value) {
         fprintf(stderr, "[ERROR] %s:%d: Failed to reset cmd counter\n", __FILE__, __LINE__);
         return -1;
     }
-    
-    dev->cmd_ctr = si1151_read_from_register(dev, SI1151_REG_RESPONSE0);
+
+    si1151_read_from_register(dev, SI1151_REG_RESPONSE0, &data);
+    dev->cmd_ctr = data; 
 
     i = 0;
     while (i < 2)
@@ -123,16 +164,18 @@ int si1151_exec_command(si1151_t *dev, uint8_t value) {
     //wait until reset command has finished executing
     if (value == SI1151_CMD_RESET_SW)
     {
-        while (((si1151_read_from_register(dev, SI1151_REG_RESPONSE0) | 0b11110000) ^ 0b11110000) != 0x0F)
+        si1151_read_from_register(dev, SI1151_REG_RESPONSE0, &data);
+        while (((data | 0b11110000) ^ 0b11110000) != 0x0F)
         {
             fprintf(stderr, "[WARNING] %s:%d: Reset hasn't finished, waiting...\n", __FILE__, __LINE__);
             sleep(5);
+            si1151_read_from_register(dev, SI1151_REG_RESPONSE0, &data);
         }
         fprintf(stderr, "[INFO] %s:%d: Sensor has been reset\n", __FILE__, __LINE__);
         return 0;
     }
-
-    if(si1151_read_from_register(dev, SI1151_REG_RESPONSE0) == (dev->cmd_ctr)) {
+    si1151_read_from_register(dev, SI1151_REG_RESPONSE0, &data);
+    if(data == (dev->cmd_ctr)) {
         fprintf(stderr, "[ERROR] %s:%d: Wrote to register without the cmd counter changing\n", __FILE__, __LINE__);
         return -1;
     }
@@ -146,21 +189,26 @@ int si1151_force_measurement(si1151_t *dev) {
 
 uint8_t si1151_read_param_data(si1151_t *dev, uint8_t param) {
     si1151_exec_command(dev, SI1151_CMD_PARAM_QUERY | param);
-    return si1151_read_from_register(dev, SI1151_REG_RESPONSE1);
+    uint8_t data = 0;
+    si1151_read_from_register(dev, SI1151_REG_RESPONSE1, &data);
+    return data;
 }
 
 uint8_t si1151_write_param_data(si1151_t *dev, uint8_t param, uint8_t value) {
     si1151_write_to_register(dev, SI1151_REG_HOST_IN0, value);
     si1151_exec_command(dev, SI1151_CMD_PARAM_SET | param);
-    
-    return si1151_read_from_register(dev, SI1151_REG_RESPONSE1);
+    uint8_t data = 0;
+    si1151_read_from_register(dev, SI1151_REG_RESPONSE1, &data);
+    return data;
 }
 
 uint16_t si1151_read_visible(si1151_t *dev) {
     if(!dev->is_autonomous) si1151_exec_command(dev, si1151_force_measurement);
-
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_HOST_OUT0);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_HOST_OUT1);
+    uint8_t data = 0;
+    si1151_read_from_register(dev, SI1151_REG_HOST_OUT0, &data);
+    uint8_t low = data; 
+    si1151_read_from_register(dev, SI1151_REG_HOST_OUT1, &data);
+    uint8_t high = data; 
     uint16_t rslt = ((uint16_t)high << 8) | low;
     dev->IR = rslt;
     
@@ -168,8 +216,12 @@ uint16_t si1151_read_visible(si1151_t *dev) {
 }
 
 uint16_t si1151_read_IR(si1151_t *dev) {
-    uint8_t low = si1151_read_from_register(dev, SI1151_REG_HOST_OUT2);
-    uint8_t high = si1151_read_from_register(dev, SI1151_REG_HOST_OUT3);
+    if(!dev->is_autonomous) si1151_exec_command(dev, si1151_force_measurement);
+    uint8_t data = 0;
+    si1151_read_from_register(dev, SI1151_REG_HOST_OUT2, &data);
+    uint8_t low = data; 
+    si1151_read_from_register(dev, SI1151_REG_HOST_OUT3, &data);
+    uint8_t high = data; 
     uint16_t rslt = ((uint16_t)high << 8) | low;
     dev->IR = rslt;
     
